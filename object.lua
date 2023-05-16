13611fc7bedc530fcdecd6150347c63ec93d84c7
MurasakiAki <akkishia88@gmail.com>
2023-05-16 15:21:53 +0200

    start object

---
+++ object.lua
@@ -0,0 +1,42 @@
+love = require('love')
+windfield = require('lib/windfield')
+
+Object = {
+    x = 0,
+    y = 0,
+    width = 0,
+    height = 0,
+    object_type = '',
+    is_breakable = false,
+    can_deal_damage = false,
+    is_movable = false,
+    health = 0,
+    damage_range_min = 0,
+    damage_range_max = 1,
+    image = ''
+}
+
+Object.__index = Object
+
+function Object:new(x, y, width, height, object_type, is_breakable,
+    can_deal_damage, is_movable, health, damage_range_min, damage_range_max, 
+    image)
+
+    local o = setmetatable({}, Object)
+
+    o.x = x
+    o.y = y
+    o.width = width
+    o.height = height
+    o.object_type = object_type
+    o.is_breakable = is_breakable
+    o.can_deal_damage = can_deal_damage
+    o.is_movable = is_movable
+    o.health = health
+    o.damage_range_min = damage_range_min
+    o.damage_range_max = damage_range_max
+    o.image = image
+
+    --collider
+
+end
